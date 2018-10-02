4451865b3cef48038a33dccd76a529c9dfeaf555
Sergii Chebotkin <sergii.chebotkin@pl.abb.com>
2018-10-02 14:09:32 +0200

    Commented changes.

--- site/role/manifests/deb_build_server.pp
+++ site/role/manifests/deb_build_server.pp
@@ -5,11 +5,11 @@ class role::deb_build_server {
       notify { "Virtualization detected": }
       if ($facts['boardproductname'] == 'VirtualBox') {
         notify { "VirtualBox detected": }
-        include profile::debb_vb_tools
+        # include profile::debb_vb_tools
       }
       elsif ($facts['blockdevice_sda_vendor'] == 'VMware') {
         notify { "VMware detected": }
-        include profile::debb_wm_tools
+        # include profile::debb_wm_tools
       }
       else {
         alert("Getting type of VM failed, vm tools skiped ")
@@ -19,11 +19,11 @@ class role::deb_build_server {
     if ($facts['lsbdistcodename'] == 'jessie') {
     # Debian jessie spec
       notify { "${::lsbdistdescription} distribute": }
-      include profile::deb8_bports
-      class { 'profile::deb8_openjdk':
-        java_version => '8',
-        kit_type     => 'jdk',
-      }
+      # include profile::deb8_bports
+      # class { 'profile::deb8_openjdk':
+      #   java_version => '8',
+      #   kit_type     => 'jdk',
+      # }
     }
     elsif ($facts['lsbdistcodename'] == 'stretch') {
     # Debian stretch spec
@@ -31,24 +31,24 @@ class role::deb_build_server {
     }
 
     # Debian base packages
-    include profile::deb_base
+    # include profile::deb_base
 
     # Debian configuration change
-    include profile::debb_config
+    # include profile::debb_config
 
     # NTP Service for Debian
-    include profile::debb_ntp
+    # include profile::debb_ntp
 
     # Python 3 + packages for Debian
-    include profile::debb_py3
+    # include profile::debb_py3
 
     # Crontab jobs manager
     # Adding fake insert
-    class { 'profile::debb_cron':
-      condition  => 'present',
-      command    => 'echo "Crontab test execution" > /tmp/cron_exec.log',
-      job_hour   => '*/1',
-      job_minute => '30',
-    }
+    # class { 'profile::debb_cron':
+    #   condition  => 'present',
+    #   command    => 'echo "Crontab test execution" > /tmp/cron_exec.log',
+    #   job_hour   => '*/1',
+    #   job_minute => '30',
+    # }
   }
 }
